35e6863961b8d1102938612a352d84cdb767c062
daniel.gaspar <dagasji@gmail.com>
2026-05-20 13:46:09 +0200

    Update eslint.config.js

--- eslint.config.js
+++ eslint.config.js
@@ -3,11 +3,12 @@ import eslintPluginPrettier from "eslint-plugin-prettier/recommended";
 import globals from "globals";
 import reactHooks from "eslint-plugin-react-hooks";
 import tseslint from "typescript-eslint";
+import next from "@next/eslint-plugin-next";
 
 export default tseslint.config(
   { ignores: ["dist", ".next", ".output"] },
   {
-    extends: [js.configs.recommended, ...tseslint.configs.recommended],
+    extends: [js.configs.recommended, ...tseslint.configs.recommended, ...next.configs.recommended],
     files: ["**/*.{ts,tsx}"],
     languageOptions: {
       ecmaVersion: 2022,
@@ -15,6 +16,7 @@ export default tseslint.config(
     },
     plugins: {
       "react-hooks": reactHooks,
+      "@next/next": next,
     },
     rules: {
       ...reactHooks.configs.recommended.rules,
